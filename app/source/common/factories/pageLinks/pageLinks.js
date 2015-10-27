f3ec3e711df0445b20ac9ab730b40c7de5e78217
Fabian Cook <fabian.cook@nownz.co.nz>
2015-10-28 11:54:09 +1300

    Fix up some problems

--- app/source/common/factories/pageLinks/pageLinks.js
+++ app/source/common/factories/pageLinks/pageLinks.js
@@ -11,16 +11,15 @@
     }
 
     PageLinks.prototype.getLinks = function (  ) {
-    var data = [
-        {"page": "Usage", "url": "#/usage"},
-        {"page": "Payments", "url": "#/billing"},
-        {"page": "UserProfile", "url": "#/userProfile"},
-        {"page": "ChangeProfile", "url": "#/changeUserProfile"},
-        {"page": "Password", "url": "#/password"},
-        {"page": "Plan", "url": "#/plan"}
-        {"page": "Order", "url": "#/order"}
-
-    ];
+        var data = [
+            {"page": "Usage", "url": "#/usage"},
+            {"page": "Payments", "url": "#/billing"},
+            {"page": "UserProfile", "url": "#/userProfile"},
+            {"page": "ChangeProfile", "url": "#/changeUserProfile"},
+            {"page": "Password", "url": "#/password"},
+            {"page": "Plan", "url": "#/plan"},
+            {"page": "Order", "url": "#/order"}
+        ];
         //return this.$http.get('/source/common/factories/pageLinks/JsonCData.json')
         //.success(function(data){
         //    //console.log(data);
